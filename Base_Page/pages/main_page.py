c816f947470c1ae6a0bef8408879521b76f5ef9a
kristinabudanova4757 <maleficenf@gmail.com>
2023-07-18 02:35:42 +0300

    изменены файлы test_main_page добавлена директория pages добавлены файлы base_page b main_page

---
+++ Base_Page/pages/main_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+
+
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
+
+    def test_guest_can_go_to_login_page(browser):
+        link = "http:/enium/sel1py.pythonanywhere.com/"
+        page = MainPage(browser,link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+        page.open()  # открываем страницу
+        page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.ID, "login_link"), "Login link is not presented"
+
